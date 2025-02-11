02cecca80de71d17c5668ea489423a425e189980
CodeGurup <[ngugipeter892@gmail.com]>
2025-02-11 20:44:44 +0300

    transpose

--- project.js
+++ project.js
@@ -68,7 +68,7 @@ const spin = () =>{
     }
 const reels = [[], [], []];
 for(let i = 0; i < COLS; i++){
-    reels.push = ([]);
+
     const reelSymbols = [...symbols];
     for(let j = 0; j < ROWS; j++){
         const randomIndex = Math.floor(Math.random()* reelSymbols.length);
@@ -81,10 +81,39 @@ for(let i = 0; i < COLS; i++){
 }
 return reels;
 };
+const transpose = (reels) => {
+    const ROWS = reels[0].length;
+    const rows = [];
+    for (let i = 0; i < ROWS; i++) {
+        rows.push([]);  
+        for (let j = 0; j < COLS; j++) {
+            rows[i].push(reels[j][i]); 
+        }
+    }
+    return rows;
+};
+
+const printRows = (rows) => {
+    for(const row of rows){
+        let rowString = "";
+        for(const [i, symbol] of row.entries()){
+            rowString += symbol;
+            if(i != row.length - 1){
+                rowString += " | ";
+            }
+        }
+        console.log(rowString);
+    }
 
 
+
+}
 
 let balance = deposit();
  const numberOfLines = getnumberOfLines();
  const bet =getbet(balance, numberOfLines);
  const reels = spin();
+ const rows = transpose(reels);
+ console.log(reels);
+ console.log(rows);
+ printRows(rows);
